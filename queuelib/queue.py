7e35c6f61313000de45a39b2045348f70723d9a8
cheng9393 <zhujiancheng@outlook.com>
2016-11-27 10:50:49 -0500

    peek function and additional interface

--- queuelib/queue.py
+++ queuelib/queue.py
@@ -188,6 +188,7 @@ class FifoSQLiteQueue(object):
     _sql_push = 'INSERT INTO queue (item) VALUES (?)'
     _sql_pop = 'SELECT id, item FROM queue ORDER BY id LIMIT 1'
     _sql_del = 'DELETE FROM queue WHERE id = ?'
+    _sql_get = 'SELECT id, item FROM queue WHERE id = ?'
 
     def __init__(self, path):
         self._path = os.path.abspath(path)
@@ -204,16 +205,35 @@ class FifoSQLiteQueue(object):
             conn.execute(self._sql_push, (item,))
 
     def pop(self):
+        result = self.peek()
+        if result is not None:
+            self.delete(result[0])
+            return result
+
+    def peek(self, id_=None):
         with self._db as conn:
-            for id_, item in conn.execute(self._sql_pop):
-                conn.execute(self._sql_del, (id_,))
-                return item
+            if id_ is None:
+                for result in conn.execute(self._sql_pop):
+                    return result
+            else:
+                for result in conn.execute(self._sql_get, (id_,)):
+                    return result
+
+    def delete(self, id_):
+        with self._db as conn:
+            conn.execute(self._sql_del, (id_,))
 
     def close(self):
         size = len(self)
         self._db.close()
         if not size:
             os.remove(self._path)
+
+    def __getitem__(self, id_):
+        return self.peek(id_)
+
+    def __delitem__(self, id_):
+        self.delete(id_)
 
     def __len__(self):
         with self._db as conn:
